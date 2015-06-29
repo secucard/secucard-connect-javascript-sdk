1546982e5e4a3b853440387a731f6bef3d58a753
Luchko Valiantsin <akmeful@gmail.com>
2015-06-29 18:12:44 +0300

    auth credentials

---
+++ src/de.secucard.connect/auth/credentials.js
@@ -0,0 +1,26 @@
+export class Credentials {
+	
+	constructor() {
+		
+		/*
+		---- basic ------
+		 */
+		this.grant_type = null;
+		this.client_id = null;
+		this.client_secret = null;
+		
+		// ---------------
+		this.refresh_token = null;
+		// ---------------
+		this.uuid = null;
+		// ---------------
+		this.code = code;
+		// ---------------
+		this.username = null;
+		this.password = null;
+		this.device = null;
+		this.deviveinfo = {name: null};
+		
+	}
+	
+}
