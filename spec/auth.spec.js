9b8a07e0af62a4e3e9034533459c3fa5a5961f9c
Luchko Valiantsin <akmeful@gmail.com>
2015-07-01 14:19:28 +0300

    add tests for the grant_type "device"

--- spec/auth.spec.js
+++ spec/auth.spec.js
@@ -2,6 +2,7 @@ import TestPromise from 'jasmine-es6/helpers/promise';
 import {catchError} from 'jasmine-es6';
 import install from 'jasmine-es6/overrides/async';
 import {Promise as ES6Promise} from 'es6-promise';
+import _ from 'lodash';
 
 import {Rest} from '../src/de.secucard.connect/net/rest'
 import {Message, HEAD, GET, POST, PUT, DELETE} from '../src/de.secucard.connect/net/message'
@@ -9,6 +10,8 @@ import {Connection} from '../src/de.secucard.connect/connection'
 import {ConnectionConfig} from '../src/de.secucard.connect/connection-config'
 import {Auth} from '../src/de.secucard.connect/auth/auth'
 import devCredentials from './support/dev-credentials.json'
+import devCredentialsRefreshToken from './support/dev-credentials-refresh-token.json'
+import devCredentialsDevice from './support/dev-credentials-device.json'
 
 install();
 
@@ -50,18 +53,12 @@ describe('Authorization', function() {
     
   });
   
-  
-  it('tries to refresh Auth Token', async function() {
-    
-    //TODO move credentials to the JSON file
-    //let devCredentials; // await fs.readFile('./spec/support/dev-credentials.json', 'utf8');
-    //console.log(devCredentials.client_id);
+  it('tries to get Auth Token with valid credentials', async function() {
     
     let status = 'getToken never called';
     
     // first send wrong data
-    
-    let pr = this.auth.getTokenRefresh(devCredentials, this.ch);
+    let pr = this.auth.getToken(devCredentials, this.ch);
     
     await pr.then((res) => {
       status = res.status;
@@ -73,5 +70,65 @@ describe('Authorization', function() {
     expect(status).toBe(200);
     
   });
+  
+  it('tries to refresh Auth Token', async function() {
+    
+    //TODO move credentials to the JSON file
+    //let devCredentials; // await fs.readFile('./spec/support/dev-credentials.json', 'utf8');
+    //console.log(devCredentials.client_id);
+    
+    let status = 'getToken never called';
+    
+    // first send wrong data
+    
+    let pr = this.auth.getTokenRefresh(devCredentialsRefreshToken, this.ch);
+    
+    await pr.then((res) => {
+      status = res.status;
+    }).catch((err)=>{
+      status = err.status;
+      console.log(err.response.body);
+    });
+    
+    expect(status).toBe(200);
+    
+  });
+  
+  it('tries to get Code for the device and get Token for that code with no success', async function() {
+    
+    // we cannot test it to get 200 because there is an user_code verification step on device
+    let status = 'getToken never called';
+    
+    // first send wrong data
+    let cr = _.assign({}, devCredentials, {uuid: "/vendor/secucard/parameter1/test1/parameter2/test2"});
+    let pr = this.auth.getDeviceCode(cr, this.ch);
+    
+    let code = null;
+    await pr.then((res) => {
+      status = res.status;
+      console.log(res.body);
+      code = res.body.device_code;
+    }).catch((err)=>{
+      status = err.status;
+      console.log(err.response.body);
+    });
+    
+    expect(status).toBe(200);
+    
+    cr = _.assign({}, devCredentialsDevice, {code: code});
+    pr = this.auth.getTokenDevice(cr, this.ch);
+    
+    await pr.then((res) => {
+      status = res.status;
+      console.log(res.body);
+    }).catch((err)=>{
+      status = err.status;
+      console.log(err.response.body);
+    });
+    
+    expect(status).toBe(401);
+    
+  });
+  
   
 });
--- src/de.secucard.connect/auth/auth.js
+++ src/de.secucard.connect/auth/auth.js
@@ -24,31 +24,31 @@ export class Auth {
 	
 	getToken(credentials, channel) {
 		let cr = _.pick(credentials, this.baseCredentialNames);
-		cr = Object.assign(cr, {grant_type: 'client_credentials'});
+		cr = _.assign({}, cr, {grant_type: 'client_credentials'});
 		return this._tokenRequest(cr, channel)
 	}
 	
 	getTokenAppUser(credentials, channel) {
 		let cr = _.pick(credentials, this.baseCredentialNames.concat(['username', 'password', 'device', 'deviceinfo']));
-		cr = Object.assign(cr, {grant_type: 'appuser'});
+		cr = _.assign({}, cr, {grant_type: 'appuser'});
 		return this._tokenRequest(cr, channel)
 	}
 	
 	getTokenRefresh(credentials, channel) {
 		let cr = _.pick(credentials, this.baseCredentialNames.concat(['refresh_token']));
-		cr = Object.assign(cr, {grant_type: 'refresh_token'});
+		cr = _.assign({}, cr, {grant_type: 'refresh_token'});
 		return this._tokenRequest(cr, channel)
 	}
 	
-	getTokenDeviceCode(credentials, channel) {
+	getDeviceCode(credentials, channel) {
 		let cr = _.pick(credentials, this.baseCredentialNames.concat(['uuid']));
-		cr = Object.assign(cr, {grant_type: 'device'});
+		cr = _.assign({}, cr, {grant_type: 'device'});
 		return this._tokenRequest(cr, channel)
 	}
 	
 	getTokenDevice(credentials, channel) {
 		let cr = _.pick(credentials, this.baseCredentialNames.concat(['code']));
-		cr = Object.assign(cr, {grant_type: 'device'});
+		cr = _.assign({}, cr, {grant_type: 'device'});
 		return this._tokenRequest(cr, channel)
 	}
 	
