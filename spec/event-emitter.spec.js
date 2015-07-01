5a44fe0edba410668f838579f941292666111c47
Luchko Valiantsin <akmeful@gmail.com>
2015-07-01 20:42:36 +0300

    basic eventemitter3 example

---
+++ spec/event-emitter.spec.js
@@ -0,0 +1,36 @@
+import EE from 'eventemitter3';
+
+class CustomEventEmitter {
+	constructor () {
+		Object.assign(this, EE.prototype);
+	}
+}
+
+describe("testing EventEmitter3", function() {
+	var originalTimeout;
+	
+	beforeEach(function() {
+		originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+	  	jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+	});
+	
+	it("takes a long time", function(done) {
+	  	
+		let emitter = new CustomEventEmitter();
+		let emitter1 = new CustomEventEmitter();
+		
+		emitter.on('customEvent', (res)=>{
+			expect(res.hello).toBe('World');
+			done();
+		});
+		
+		emitter1.emit('customEvent', {hello: "World1"});
+		emitter.emit('customEvent', {hello: "World"});
+		
+	});
+	
+	afterEach(function() {
+	  	jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+	});
+	
+});
