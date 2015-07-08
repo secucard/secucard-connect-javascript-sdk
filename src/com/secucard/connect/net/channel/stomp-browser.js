320d5fbe3aef5ee45c2647e6ecda96a093c3f349
Mitz Suyi <mitzsuyi@no-reply.com>
2015-07-08 20:44:47 +0700

    added helper methods to stomp browser

--- src/com/secucard/connect/net/channel/stomp-browser.js
+++ src/com/secucard/connect/net/channel/stomp-browser.js
@@ -1,9 +1,54 @@
 import StompJS from 'stomp-websocket'
 import SockJSClient from 'sockjs-client'
 
+var TEMP_QUEUE = '/temp-queue/main'
+
+var notifyListener = function(event, details) {
+  if (this._listener) {
+    this._listener(event, details)
+  }
+}
+
+var tempQueue = function() {
+  return TEMP_QUEUE
+}
+
+var header = function(requestId, options) {
+  var _header = {
+    'user-id': this.accessToken,
+    'reply-to': tempQueue(),
+    'correlation-id': requestId
+  }
+  if (options.appId) {
+    _header["app-id"] = options.appId
+  }
+  return _header
+}
+
 export class StompBrowser {
   constructor(wsUrl) {
-    return new StompJS.over(new SockJSClient.SockJS(wsUrl))
+    this.client =  StompJS.over(new SockJSClient.SockJS(wsUrl))
+    this._listener = null
+    return this
   }
-
+  listener(listener) {
+    this._listener
+    return this
+  }
+  notifyListener(event, details) {
+    notify.call(event, details)
+  }
+  requestHeader() {
+    header.call(this)
+  } 
+  request(token, destination, requestId, options) {
+    this.client.connect(token, token, (frame) => {
+      notifyListener("connected")
+      this.client.subscribe(destination, (message) => {
+         var type = message.correlationId ? "message" : "event"
+         notifyListener(type, message)
+      })
+      this.client(send, requestHeader(requestId, options), payload)
+    })
+  }
 }
