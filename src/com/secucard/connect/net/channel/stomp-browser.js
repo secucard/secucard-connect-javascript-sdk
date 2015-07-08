acd91f66c31debe6036840b849c18f88e36eeb3d
Mitz Suyi <mitzsuyi@no-reply.com>
2015-07-08 13:25:25 +0700

    quick fixes
    stompjs now stomp-websocket, socksjs is sockjsclient.sockjs

--- src/com/secucard/connect/net/channel/stomp-browser.js
+++ src/com/secucard/connect/net/channel/stomp-browser.js
@@ -1,9 +1,9 @@
-import Stomp from 'stompjs'
-import SockJS from 'sockjs-client'
+import StompJS from 'stomp-websocket'
+import SockJSClient from 'sockjs-client'
 
 export class StompBrowser {
   constructor(wsUrl) {
-    return new Stomp.over(new SockJS(wsUrl))
+    return new StompJS.over(new SockJSClient.SockJS(wsUrl))
   }
 
 }
