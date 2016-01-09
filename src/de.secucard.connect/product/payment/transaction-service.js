d86dfa09567b93ebd401830af534aa3740223d90
Valiantsin Luchko <akmeful@gmail.com>
2016-01-09 11:34:28 +0300

    New(payment.transactions): add cancel and getShippingUrl actions

--- src/de.secucard.connect/product/payment/transaction-service.js
+++ src/de.secucard.connect/product/payment/transaction-service.js
@@ -16,6 +16,14 @@ export class TransactionService extends ProductService {
     constructor() {
         super()
     }
+    
+    getShippingUrl(id) {
+        return this.retrieveWithAction(id, 'shippingUrl');
+    }
+    
+    cancel(id, data) {
+        return this.execute(id, 'cancel', null, data);
+    }
 
     getEndpoint() {
         return ['payment', 'transactions'];
