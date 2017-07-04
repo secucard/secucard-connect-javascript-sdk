22c7c7ddf42620fd6a2efb0960876c349abb86da
Rico Simlinger <r.simlinger@secupay.ag>
2017-07-04 13:28:47 +0200

    UPDATE product payment

--- src/de.secucard.connect/product/payment/model/transaction.js
+++ src/de.secucard.connect/product/payment/model/transaction.js
@@ -1,26 +1,27 @@
 /*
- Copyright 2015 hp.weber GmbH & Co secucard KG (www.secucard.com)
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
+ * SDK-Generator version: 1.1.0
  */
 export class Transaction {
-
-    constructor(customer, contract, amount, currency, purpose, order_id, trans_id, status, transaction_status) {
-        this.customer = customer;
-        this.contract = contract;
-        this.amount = amount;
-        this.currency = currency;
-        this.purpose = purpose;
-        this.order_id = order_id;
-        this.trans_id = trans_id;
-        this.status = status;
-        this.transaction_status = transaction_status;
-    }
-
+  constructor(
+    purpose, customer, recipient, basket, experience, accrual, subscription, redirectUrl, optData, paymentAction,
+    contract, amount, currency, orderId, transId, status, transactionStatus, usedPaymentInstrument) {
+    this['purpose'] = purpose;
+    this['customer'] = customer;
+    this['recipient'] = recipient;
+    this['basket'] = basket;
+    this['experience'] = experience;
+    this['accrual'] = accrual;
+    this['subscription'] = subscription;
+    this['redirect_url'] = redirectUrl;
+    this['opt_data'] = optData;
+    this['payment_action'] = paymentAction;
+    this['contract'] = contract;
+    this['amount'] = amount;
+    this['currency'] = currency;
+    this['order_id'] = orderId;
+    this['trans_id'] = transId;
+    this['status'] = status;
+    this['transaction_status'] = transactionStatus;
+    this['used_payment_instrument'] = usedPaymentInstrument;
+  }
 }
